b4e703b86874f29cbbe5d9a168fcf65e23230323
GauravSingh789 <gauravs.nitt@gmail.com>
2022-02-13 12:54:36 +0530

    modified dot_prod module

--- RTL/mac.v
+++ RTL/mac.v
@@ -19,21 +19,21 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-(* use_dsp = "yes" *) module dot_prod #(parameter DBUS_LENGTH = 16, parameter XLEN_PIXEL = 8 , parameter NUM_OF_PIXELS = 900)
+(* use_dsp = "yes" *) module dot_prod #(parameter XLEN_PIXEL = 8 , parameter NUM_OF_PIXELS = 900)
 ( input clk, rst,
-  input [DBUS_LENGTH-1 : 0] x_test,  //Doubtful on the input port size.
-  input [DBUS_LENGTH-1 : 0] x_sv,
-  output [2*DBUS_LENGTH -1 : 0] mac_out); // (2^16)*2^10 = 2^26 (so, 26 bits. Taking the size as 32) (roughly 900 taken as 2^10)
+  input [XLEN_PIXEL-1 : 0] x_test, 
+  input [XLEN_PIXEL-1 : 0] x_sv,
+  output [4*XLEN_PIXEL -1 : 0] mac_out); // (2^16)*2^10 = 2^26 (so, 26 bits. Taking the size as 32) (roughly 900 taken as 2^10)
   
   reg [XLEN_PIXEL-1:0] x_test_arr [0:NUM_OF_PIXELS];
   reg [XLEN_PIXEL-1:0] x_sv_arr [0:NUM_OF_PIXELS];  
-  reg [2*DBUS_LENGTH -1 : 0] result; 
+  reg [4*XLEN_PIXEL -1 : 0] result; 
   reg k,c_done;
   
   integer i;
   always @ (x_test)
   begin
-      for (i=0; i< NUM_OF_PIXELS; i=i+1) begin
+      for (i=0; i < NUM_OF_PIXELS; i=i+1) begin
           x_test_arr[i] = x_test[i*XLEN_PIXEL +: XLEN_PIXEL];
       end
   end
